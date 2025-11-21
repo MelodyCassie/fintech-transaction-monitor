86fa62df37c42cc7e25c9c1cc0d36767b132358a
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 16:56:02 +0100

    feat: add reactive REST endpoints for transaction operations

--- transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
@@ -1,10 +1,7 @@
 package com.fintech.transaction_service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
