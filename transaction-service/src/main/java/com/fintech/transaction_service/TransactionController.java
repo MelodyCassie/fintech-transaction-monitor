a8eb9f69211afafb4b901880d6cc470459057860
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 16:47:30 +0100

    feat: add reactive REST controller with WebFlux endpoints

--- transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
@@ -1,8 +1,10 @@
 package com.fintech.transaction_service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/transaction")
@@ -11,4 +13,8 @@ public class TransactionController {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    @GetMapping
+    public Flux<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
 }
