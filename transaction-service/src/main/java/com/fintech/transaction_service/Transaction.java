20e7f21b52d45a6115a9178fe0a663c7881ebd71
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 17:57:33 +0100

    fix: add constructor for json deserialization

--- transaction-service/src/main/java/com/fintech/transaction_service/Transaction.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/Transaction.java
@@ -1,7 +1,9 @@
 package com.fintech.transaction_service;
 
 import com.fasterxml.jackson.databind.node.DoubleNode;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table("transactions")
 public class Transaction {
     @Id
@@ -19,7 +23,11 @@ public class Transaction {
     private String description;
     private LocalDateTime timestamp;
 
-    public Transaction() {
+    public Transaction(Double amount, String fromAccount, String toAccount, String description) {
+        this.amount = amount;
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.description = description;
         this.timestamp = LocalDateTime.now();
     }
 
