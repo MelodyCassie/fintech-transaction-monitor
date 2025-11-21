ebedee14993421fd32270854d7e376c98ba3c852
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 22:12:48 +0100

    feat: inject NotificationService into NotificationController class

--- notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
+++ notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
@@ -1,21 +1,23 @@
 package com.fintech.notification_service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("/alerts")
 public class NotificationController{
 
+    @Autowired
+    private NotificationService notificationService;
+
     @PostMapping
     public Mono<Alert> createAlert(@RequestBody Alert alert) {
-        System.out.println("ALERT: " + alert.getMessage());
-        return Mono.just(alert);
+        log.info("Received alert request: {}", alert.getMessage());
+        return notificationService.sendAlert(alert);
     }
 
-    @GetMapping("/test")
-    public Mono<String> test() {
-        return Mono.just("Notification service is working!");
-    }
 
 }
