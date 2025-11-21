7f78eaaf7c2d1767b70ca968e06487a28e0fcb68
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 21:33:14 +0100

    feat: add necessary annotations for NotificationController class

--- notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
+++ notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
@@ -1,4 +1,9 @@
 package com.fintech.notification_service;
 
-public class NotificationController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alerts")
+public class NotificationController{
 }
