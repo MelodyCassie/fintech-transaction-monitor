3cc5155668b42eb94c216614e3b99c4e2566d719
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 16:30:40 +0100

    feat: add annotation to Transaction class

--- transaction-service/src/main/java/com/fintech/transaction_service/Transaction.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/Transaction.java
@@ -1,10 +1,14 @@
 package com.fintech.transaction_service;
 
 import com.fasterxml.jackson.databind.node.DoubleNode;
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Data
+@Table("transactions")
 public class Transaction {
     private Long id;
     private Double amount;
