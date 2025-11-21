828debd9798845d2529181055bbebaef9bc3bbe1
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 21:45:34 +0100

    feat: add createAlert in controller class

--- notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
+++ notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
@@ -1,9 +1,19 @@
 package com.fintech.notification_service;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/alerts")
 public class NotificationController{
+
+    @PostMapping
+    public Mono<Alert> createAlert(@RequestBody Alert alert) {
+        System.out.println("🚨 ALERT: " + alert.getMessage());
+        return Mono.just(alert);
+    }
+
 }
