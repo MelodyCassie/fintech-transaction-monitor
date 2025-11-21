1596e1d34a8bb954f6486423841c1fec0c4ef1af
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 20:55:53 +0100

    feat: add annotation

--- notification-service/src/main/java/com/fintech/notification_service/Alert.java
+++ notification-service/src/main/java/com/fintech/notification_service/Alert.java
@@ -1,9 +1,13 @@
 package com.fintech.notification_service;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class Alert {
     private String message;
     private String transactionId;
     private LocalDateTime timeStamp;
+
 }
