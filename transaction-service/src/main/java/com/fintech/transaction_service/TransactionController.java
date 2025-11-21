4207608d11ce5c8f1f7fd693e277711f3d488adb
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 16:44:28 +0100

    feat: inject TransactionRepository in controller class

--- transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
@@ -1,6 +1,14 @@
 package com.fintech.transaction_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/transaction")
 public class TransactionController {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
 
 }
