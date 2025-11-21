c7f83d15f35cfdd3e8c749dfbc94c9e5e31e3311
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 22:07:52 +0100

    feat: add send alert method in Notification method

--- notification-service/src/main/java/com/fintech/notification_service/NotificationService.java
+++ notification-service/src/main/java/com/fintech/notification_service/NotificationService.java
@@ -1,4 +1,14 @@
 package com.fintech.notification_service;
 
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
 public class NotificationService {
+
+    public Mono<Alert> sendAlert(Alert alert) {
+
+        System.out.println("🔔 SENDING ALERT: " + alert.getMessage());
+        return Mono.just(alert);
+    }
 }
