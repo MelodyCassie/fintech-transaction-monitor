a8e7c66fa05b46cc1fe0e933c6e9454f9b70f05a
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 20:53:52 +0100

    feat: add private fields

---
+++ notification-service/src/main/java/com/fintech/notification_service/Alert.java
@@ -0,0 +1,9 @@
+package com.fintech.notification_service;
+
+import java.time.LocalDateTime;
+
+public class Alert {
+    private String message;
+    private String transactionId;
+    private LocalDateTime timeStamp;
+}
