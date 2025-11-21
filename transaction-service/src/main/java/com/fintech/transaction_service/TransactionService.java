a398a0a6a117dbfc2207b7d55fb30178d7083917
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-21 21:36:21 +0100

    feat: update  TransactionService class

--- transaction-service/src/main/java/com/fintech/transaction_service/TransactionService.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/TransactionService.java
@@ -1,5 +1,24 @@
 package com.fintech.transaction_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
 public class TransactionService {
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Flux<Transaction> getAllTransactions(){
+        return transactionRepository.findAll();
+    }
+    public Mono<Transaction> createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Flux<Transaction> getLargeTransactions() {
+        return transactionRepository.findByAmountGreaterThan(5000.0);
+    }
 }
