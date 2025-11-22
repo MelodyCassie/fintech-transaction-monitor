794f6cebed9234d7bfecf4cf3129351fd717d536
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-22 12:11:36 +0100

    fix: remove testing log from TransactionController class

--- transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/TransactionController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
@@ -16,25 +15,21 @@ public class TransactionController {
 
     @GetMapping
     public Flux<Transaction> getAllTransactions() {
-        log.info("GET /transactions called");
         return transactionService.getAllTransactions();
     }
 
     @PostMapping
     public Mono<Transaction> createTransaction(@RequestBody Transaction transaction) {
-        log.info("POST /transactions called with: {}", transaction);
         return transactionService.createTransaction(transaction);
     }
 
     @GetMapping("/large")
     public Flux<Transaction> getLargeTransactions() {
-        log.info("GET /transactions/large called");
         return transactionService.getLargeTransactions();
     }
 
     @GetMapping("/test")
     public Mono<String> testEndpoint() {
-        log.info("TEST endpoint called");
-        return Mono.just("Transaction service is working!");
+        return Mono.just("Transaction service is working");
     }
 }
