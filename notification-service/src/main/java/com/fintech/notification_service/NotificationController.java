520d9a73629620daf933d142f837e9b2ad9822dd
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-24 23:14:21 +0100

    feat:

--- notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
+++ notification-service/src/main/java/com/fintech/notification_service/NotificationController.java
@@ -1,6 +1,5 @@
 package com.fintech.notification_service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
